a9ff97c6b87d1f7a19cbf8e6d6dcf6cdbd946744
Abdulhakim Kullow <abdulhakim.kullow@student.moringaschool.com>
2024-03-31 17:00:31 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+const divide = () => {
+    return 2000/100;
+}
+const square = (param1) => {
+      return param1*param1;
+}
+const add = (parameter1, parameter2) => {
+     return parameter1 + parameter2;
+}
